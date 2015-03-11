8d9dbf6feffb3e517343f08050d2f960d1688ee8
QAgate iMac4 <qagatetechnologies@QAgates-Mac-mini-4.local>
2015-03-11 16:04:24 +0530

    Initial Commit

---
+++ iwatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iwatch
+//
+//  Created by QAgate iMac4 on 11/03/15.
+//  Copyright (c) 2015 QAgate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
